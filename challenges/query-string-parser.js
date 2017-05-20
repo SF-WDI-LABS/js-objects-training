adc6f65bb6c53ebb6f9c96495351c3ea08f84622
bschuetzle <bill_schuetzle@sbcglobal.net>
2017-05-19 17:54:55 -0700

    first version of query string parser

--- challenges/query-string-parser.js
+++ challenges/query-string-parser.js
@@ -44,4 +44,37 @@
 
 */
 
-// YOUR CODE HERE
+function parseQueryString(str) {
+  // split the string into an array of key-value pair strings
+  strArr = str.split("&");
+  //console.log(strArr);
+  // declare an empty string that will hold the completed string (formatted as object)
+  var returnStr = "";
+  // add the opening curly bracket
+  returnStr = returnStr.concat("{");
+  // iterate through each key-value pair strings
+  strArr.forEach(function(currentVal, index) {
+    // for the first element, add a new line
+    if (index === 0) {
+      returnStr = returnStr.concat("\n");
+    }
+    // split the string into an array with the key and value (key at index 0, value at index 1)
+    currentVal = currentVal.split("=");
+    // add a line formatted as "key": "value"
+    returnStr = returnStr.concat(`\"${currentVal[0]}\": \"${currentVal[1]}\"`);
+    // for the last element, just add a new line, otherwise add both a comma and a new line
+    if (index === strArr.length - 1) {
+      returnStr = returnStr.concat("\n");
+    } else {
+      returnStr = returnStr.concat(",\n");
+    }
+  })
+  // add the closing curly bracket
+  returnStr = returnStr.concat("}");
+  // return the final string as an object
+  return returnStr;
+}
+
+var result = parseQueryString("first=alpha&last=omega");
+console.log(JSON.parse(result));
+//console.log(JSON.parse(result));
