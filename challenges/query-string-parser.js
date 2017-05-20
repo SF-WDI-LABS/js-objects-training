3667052ca207e9863926a334ad3977da014bf7ae
Ryan <rkellychamber@gmail.com>
2017-05-20 12:21:34 -0700

    Update query-string-parser.js

--- challenges/query-string-parser.js
+++ challenges/query-string-parser.js
@@ -45,3 +45,16 @@
 */
 
 // YOUR CODE HERE
+	function QueryStringToJSON() {			
+			var pairs = location.search.slice(1).split('&');
+			
+			var result = {};
+			pairs.forEach(function(pair) {
+				pair = pair.split('=');
+				result[pair[0]] = decodeURIComponent(pair[1] || '');
+			});
+
+			return JSON.parse(JSON.stringify(result));
+		}
+		
+		var qs = QueryStringToJSON();
